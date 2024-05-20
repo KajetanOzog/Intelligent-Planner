a0509136b9627f652c630f38b59721cbb4142c5c
Kajetan <kajetanozog@gmail.com>
2024-05-20 15:37:39 +0200

    zmiana daty

--- app/src/main/java/com/example/io_project/datamanagement/addEvent.kt
+++ app/src/main/java/com/example/io_project/datamanagement/addEvent.kt
@@ -1,11 +1,11 @@
 import com.example.io_project.dataclasses.Event
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.DayOfWeek
+import java.util.Locale
 
 suspend fun addEventToFirestore(userID: String, event: Event, isRegular: Boolean) {
     val firestore = FirebaseFirestore.getInstance()
@@ -26,21 +26,25 @@ suspend fun addEventToFirestore(userID: String, event: Event, isRegular: Boolean
         "description" to event.description
     )
 
-    if (isRegular) {
-        val dayOfWeek = getDayOfWeek(event.date)
-        val regularEventsRef = userDocumentRef.collection("regular")
-        val dayEventsRef = regularEventsRef.document(dayOfWeek)
-        dayEventsRef.update("events", FieldValue.arrayUnion(eventData))
-    } else {
-        val nonRegularEventsRef = userDocumentRef.collection("nonregular")
-        val dateEventsRef = nonRegularEventsRef.document(event.date)
-        dateEventsRef.update("events", FieldValue.arrayUnion(eventData))
+    try {
+        if (isRegular) {
+            val dayOfWeek = getDayOfWeek(event.date)
+            val regularEventsRef = userDocumentRef.collection("regular")
+            val dayEventsRef = regularEventsRef.document(dayOfWeek)
+            dayEventsRef.update("events", FieldValue.arrayUnion(eventData)).await()
+        } else {
+            val nonRegularEventsRef = userDocumentRef.collection("nonregular")
+            val dateEventsRef = nonRegularEventsRef.document(event.date)
+            dateEventsRef.update("events", FieldValue.arrayUnion(eventData)).await()
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
     }
 }
 
 
 fun getDayOfWeek(date: String): String {
-    val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+    val formatter = DateTimeFormatter.ofPattern("EEE, MMM d yyyy", Locale.ENGLISH)
     val localDate = LocalDate.parse(date, formatter)
     val dayOfWeek = localDate.dayOfWeek
     return when (dayOfWeek) {
