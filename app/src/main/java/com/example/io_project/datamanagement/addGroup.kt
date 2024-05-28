89341e67cdb9a2826a5f957944dc239db7cfb1dc
Kajetan <kajetanozog@gmail.com>
2024-05-28 21:47:23 +0200

    Dodawanie grup i zmiana lekka w user_emial

--- app/src/main/java/com/example/io_project/datamanagement/addGroup.kt
+++ app/src/main/java/com/example/io_project/datamanagement/addGroup.kt
@@ -2,6 +2,8 @@ package com.example.io_project.datamanagement
 
 import com.example.io_project.dataclasses.Group
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import kotlinx.coroutines.tasks.await
 import java.util.UUID
 
 //Przekazujcie mi grupe ale bez groupID (mozecie dac na ""), ja je sam ustawie
