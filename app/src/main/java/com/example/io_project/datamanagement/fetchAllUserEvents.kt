70428b0b2fd852d618591da3f444931736146f78
Kajetan <kajetanozog@gmail.com>
2024-06-12 11:23:10 +0200

    naprawa dziedziczenia

--- app/src/main/java/com/example/io_project/datamanagement/fetchAllUserEvents.kt
+++ app/src/main/java/com/example/io_project/datamanagement/fetchAllUserEvents.kt
@@ -1,34 +1,5 @@
 package com.example.io_project.datamanagement
 
-/*suspend fun fetchAllUserEvents(userID: String, date: String): List<Event>? {
-    val allEvents = ArrayList<Event>()
-
-    // Fetch events from friends
-    val friendsEvents: List<Event>? = fetchFriendsEvents(userID, date)
-    friendsEvents?.let { allEvents.addAll(it) }
-
-    // Fetch user's own events
-    val userEvents: List<Event>? = fetchEvents(userID, date)
-    userEvents?.let { allEvents.addAll(it) }
-
-    // Fetch events from groups
-    val groupsEvents: Map<String, List<Event>>? = fetchGroupsEvents(userID)
-    groupsEvents?.forEach { (_, events) ->
-        events.filter { it.date == date }.let { allEvents.addAll(it) }
-    }
-
-    // Sort events by time
-    val timeFormat = SimpleDateFormat("HH:mm", Locale.ENGLISH)
-    try {
-        allEvents.sortWith(compareBy { event ->
-            timeFormat.parse(event.time)
-        })
-    } catch (e: ParseException) {
-        Log.d("FetchAllUserEvents", "Error parsing time: ${e.message}")
-    }
-
-    return if (allEvents.isEmpty()) null else allEvents
-}*/
 
 
 import android.util.Log
--- app/src/main/java/com/example/io_project/datamanagement/fetchGroupEvents.kt
+++ app/src/main/java/com/example/io_project/datamanagement/fetchGroupEvents.kt
@@ -6,26 +6,6 @@ import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.Locale
 
-/*
-suspend fun fetchGroupsEvents(userID: String): Map<String, List<Event>>? {
-    val groups = fetchUserGroups(userID)
-    val groupEventsMap = mutableMapOf<String, List<Event>>()
-    try {
-        if (groups != null) {
-            for (group in groups) {
-                groupEventsMap[group.groupID] = group.events
-            }
-            return groupEventsMap
-        } else {
-            Log.d("FetchGroupsEvents", "No groups found for the user.")
-        }
-    } catch (e: Exception) {
-        Log.d("FetchGroupsEvents", "Error while fetching group events: ${e.message}")
-    }
-    return null
-}
-*/
-
 suspend fun fetchGroupsEvents(userID: String): Map<String, List<Event>>? {
     val groups = fetchUserGroups(userID)
     val groupEventsMap = mutableMapOf<String, List<Event>>()
