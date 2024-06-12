8e93ba6732a6cd9dc698d181185fc5185f2d6c67
Michał Smorągiewicz <michalgsmoragiewicz@gmail.com>
2024-06-12 22:21:14 +0200

    Intelligent Planner v1.0

--- app/src/main/java/com/example/io_project/ui/dialogs/AddGoalViewModel.kt
+++ app/src/main/java/com/example/io_project/ui/dialogs/AddGoalViewModel.kt
@@ -8,8 +8,6 @@ import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
